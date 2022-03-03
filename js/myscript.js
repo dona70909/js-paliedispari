0ef179f74208c7872f016074b5e91fdfaf03840a
Donatella7 <campaniellodonatella@gmail.com>
2022-03-03 15:27:48 +0100

    pari o dispari

--- js/myscript.js
+++ js/myscript.js
@@ -12,7 +12,6 @@ console.log(arrayChar);
 
 function checkPali(arrayOfChar){
     for(let i = 0; i<arrayOfChar.length; i++){
-
         for(let c = arrayOfChar.length -1 ; c < arrayOfChar.length ; c-- ){
 
             if (arrayOfChar[i] === arrayOfChar[c]){
@@ -26,3 +25,40 @@ function checkPali(arrayOfChar){
 
 console.log(checkPali(arrayChar));
 
+/* pari o dispari */
+
+let myselect = document.getElementById("my-select");
+let choice = myselect.value;
+
+const userNumber = document.getElementById("my-number").value;
+
+const pcNumberPrint = document.getElementById("pc-number-output");
+
+
+function randomNumber(max,min){
+    return(Math.floor(Math.random() * (max - min) - min));
+}
+
+function evenOdd(number){
+    if((number % 2) == 0){
+        return true;
+    }
+
+    return false;
+}
+
+if((userNumber >= 1) && (userNumber <= 5)){
+    const pcNumberValue = randomNumber(5,1);
+    pcNumberPrint.innerHTML = (pcNumberValue);
+    let sum = userNumber + pcNumberValue;
+    evenOdd(sum);
+
+    if((evenOdd == true) && (choice.value == "Pari")){
+        document.getElementById("my-score").innerHTML == "Hai vinto";
+    } else if ((evenOdd == false) && (choice.value == "Dispari")){
+
+        document.getElementById("my-score").innerHTML ="Hai vinto";
+    } else {
+        document.getElementById("my-score").innerHTML = "Hai perso";
+    }
+}
