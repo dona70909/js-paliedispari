2fceda117546deb4bed4bcbc94a591b9937d9cf4
Donatella7 <campaniellodonatella@gmail.com>
2022-03-03 16:05:44 +0100

    check with the btn

--- js/myscript.js
+++ js/myscript.js
@@ -1,29 +1,30 @@
 
 /* From html */
-const userWord = document.getElementById("my-input").value;
-
-const arrayChar = [];
-
-for(let i = 0; i<userWord.length; i++){
-    arrayChar.push(userWord.at(i));
-}
-
-console.log(arrayChar);
-
 function checkPali(arrayOfChar){
     for(let i = 0; i<arrayOfChar.length; i++){
         for(let c = arrayOfChar.length -1 ; c < arrayOfChar.length ; c-- ){
-
+            
             if (arrayOfChar[i] === arrayOfChar[c]){
                 return true
             } 
-    
+            
             return false;
         }
     }
 }
 
-document.getElementById("my-output-pali").innerHTML = checkPali(arrayChar);
+const btnCheck = document.getElementById("my-btn-check");
+console.log(btnCheck);
+btnCheck.addEventListener("click",function(){
+    const userWord = document.getElementById("my-input").value;
+    const arrayChar = [];
+    for(let i = 0; i<userWord.length; i++){
+        arrayChar.push(userWord.at(i));
+    }
+    document.getElementById("my-output-pali").innerHTML = checkPali(arrayChar);
+});
+
+
 
 // % PARI O DISPARI 
 
@@ -43,7 +44,7 @@ function evenOdd(number){
     if((number % 2) == 0){
         return true;
     }
-
+    
     return false;
 }
 
@@ -52,11 +53,11 @@ if((userNumber >= 1) && (userNumber <= 5)){
     pcNumberPrint.innerHTML = (pcNumberValue);
     let sum = userNumber + pcNumberValue;
     evenOdd(sum);
-
+    
     if((evenOdd == true) && (choice.value == "Pari")){
         document.getElementById("my-score").innerHTML == "Hai vinto";
     } else if ((evenOdd == false) && (choice.value == "Dispari")){
-
+        
         document.getElementById("my-score").innerHTML ="Hai vinto";
     } else {
         document.getElementById("my-score").innerHTML = "Hai perso";
